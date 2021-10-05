e01517736825beee689d77d821799410d8877a55
Gary J <github@accu.ca>
2021-10-05 15:33:48 -0700

    Add Express route

--- src/server.js
+++ src/server.js
@@ -6,6 +6,28 @@ const port = process.env.PORT || 8000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "labber",
+  password: "labber",
+  host: "localhost",
+  database: "pets",
+});
+
+
+app.get("/pets", (req, res) => {
+  const sql = "select pets.*, owners.name as owner, \
+    animals.name as animal from pets \
+    join owners on owner_id=owners.id \
+    join a
+    nimals on animal_id=animals.id";
+
+  return pool.query(sql)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => console.log(err.message));
+});
 
 app.get("/hello", (req, res) => {
   res.send("Hello Express World");
