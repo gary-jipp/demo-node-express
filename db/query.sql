08d2933362a38501ab590779d8818dabf6d641e7
Gary J <github@accu.ca>
2021-10-05 13:10:51 -0700

    Update

--- db/query.sql
+++
@@ -1,3 +0,0 @@
- select * from pets 
-  join owners on owner_id=owners.id
-  join animals on animal_id=animals.id ;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 8002;
+const port = process.env.PORT || 8000;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 
+app.get("/hello", (req, res) => {
+  res.send("Hello Express World");
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
