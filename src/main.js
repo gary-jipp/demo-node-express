60636ce0ad1cd16ed48c35167ad9e4f00c8362d9
Gary J <github@accu.ca>
2021-10-05 14:57:07 -0700

    Update

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@
 const getPets = function(client) {
 
   const sql = "select pets.*, owners.name as owner, \
-  animals.name as animal from pets \
-  join owners on owner_id=owners.id \
-  join animals on animal_id=animals.id";
+    animals.name as animal from pets \
+    join owners on owner_id=owners.id \
+    join animals on animal_id=animals.id";
 
   return client.query(sql)
     .then(res => {
